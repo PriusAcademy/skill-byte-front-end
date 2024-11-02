a186ac3f99ceb26daa3726161c17ac4651d9d10e
ASHOK P <rajesh99ed@gmail.com>
2024-11-02 11:29:13 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base : "/vite-react-deploy/",
+  base : "/academy/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),

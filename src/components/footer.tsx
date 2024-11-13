6be5ef35c5ef667ca5b5e8233259c22b47315cc9
ASHOK P <rajesh99ed@gmail.com>
2024-11-13 10:44:15 +0530

    Fotter added

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className="bg-secondaryBlue p-4 font-semibold text-white text-center">
-      Prius IT Services &copy; Copyright 2024
+      Prius IT Services &copy; Copyright 2024.
     </div>
   );
 };
